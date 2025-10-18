f271e564ec261d7070cf190d9d64e979be388480
phc5 <paulhyunchong@gmail.com>
2025-10-18 07:26:51 -0700

    Adds Product Viewer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
+import ProductViewer from './components/ProductViewer';
 
 export default function App() {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductViewer />
     </main>
   );
 }
