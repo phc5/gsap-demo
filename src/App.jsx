104b2ff1b1e0b64c12ee6781bce7306841e24379
phc5 <paulhyunchong@gmail.com>
2025-10-18 08:26:48 -0700

    Adds showcase section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import ProductViewer from './components/ProductViewer';
+import Showcase from './components/Showcase';
 import { ScrollTrigger } from 'gsap/all';
 import gsap from 'gsap';
 
@@ -12,6 +13,7 @@ export default function App() {
       <NavBar />
       <Hero />
       <ProductViewer />
+      <Showcase />
     </main>
   );
 }
